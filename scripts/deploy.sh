dc1c06a4b0635add6946d2c532f99563a30252ff
Andrea Condoluci <andrea.condoluci@tweag.io>
2020-04-26 11:36:13 +0000

    First release

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -eu
 
-if [ -z ${TRAVIS_TAG+x} ]
+if [ -z ${TRAVIS_TAG+x} ] || [ -z "$TRAVIS_TAG" ]
 then
-  echo "$TRAVIS_TAG unbound, skipping deploy.sh"
+  echo "TRAVIS_TAG unbound, skipping deploy.sh"
 else
-  DEB="random-sleep_$TRAVIS_TAG"
+  DEB="random-sleep"
   mkdir -p $DEB/DEBIAN
   cp scripts/control $DEB/DEBIAN/
   sed -i "2s/.*/Version:\ $TRAVIS_TAG/" $DEB/DEBIAN/control
   mkdir -p $DEB/usr/local/bin
   cp $(stack path --local-install-root)/bin/rsleep $DEB/usr/local/bin/
-  dpkg-deb --build "random-sleep_$TRAVIS_TAG"
+  dpkg-deb --build $DEB
 fi
